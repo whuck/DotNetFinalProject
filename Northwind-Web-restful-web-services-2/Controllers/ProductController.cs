3272f7c3f2b5f90bace7ed579dad6d97b79d2c7d
Joshua Below <33503583+jbelow@users.noreply.github.com>
2021-05-02 16:02:16 -0500

    not working to use real data yet but set for up for it

--- Northwind-Web-restful-web-services-2/Controllers/ProductController.cs
+++ Northwind-Web-restful-web-services-2/Controllers/ProductController.cs
@@ -15,6 +15,11 @@ namespace Northwind.Controllers
             return View(_northwindContext.Categories.OrderBy(c => c.CategoryName));
         }
 
-        public IActionResult Product() => View();
+        // public IActionResult Product(int id){
+        //     ViewBag.id = id;
+        //     return View(_northwindContext.Products.Where(p => p.ProductId = id));
+        // }
+        public IActionResult Product(int id) => View(_northwindContext.Products.Where(p => p.ProductId == id));
+
     }
 }
