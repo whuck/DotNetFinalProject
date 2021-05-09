0608de35f11ec2681c7c77e163fb36bd4bfcc83f
Nick Saeger <nsaeger1@my.wctc.edu>
2021-05-09 16:47:25 -0500

    New changes

---
+++ Models/ReviewJSON.cs
@@ -0,0 +1,11 @@
+namespace Northwind.Models
+{
+    public class ReviewJSON
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public int rating { get; set; }
+        public string description {get;set;}
+        
+    }
+}
